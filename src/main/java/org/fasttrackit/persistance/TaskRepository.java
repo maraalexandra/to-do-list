8de84922d5eb01116e7793ac8bec7889776445e9
maraalexandra <mara.alexandra11@yahoo.com>
2020-05-04 21:37:28 +0300

    prepared statement

---
+++ src/main/java/org/fasttrackit/persistance/TaskRepository.java
@@ -0,0 +1,28 @@
+package org.fasttrackit.persistance;
+
+import org.fasttrackit.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TaskRepository {
+
+    public void createTask(CreateTaskRequest request) throws IOException, SQLException {
+        //preventing SQL Injection by using PreparedStatement
+        String sql = "INSERT INTO task (description, deadline) VALUES (?, ?)" ;
+
+       Connection connection = DatabaseConfiguration.getConnection();
+       connection.prepareStatement(sql);
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, request.getDescription());
+        preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
+
+
+        preparedStatement.executeUpdate();
+
+    }
+}
