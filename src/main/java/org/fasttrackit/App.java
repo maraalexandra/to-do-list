0b705fbd02504ad333f01c0f2de18b91a017c8c8
maraalexandra <mara.alexandra11@yahoo.com>
2020-05-11 21:07:51 +0300

    added Task service

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,11 +1,14 @@
 package org.fasttrackit;
 
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.persistance.TaskRepository;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Hello world!
@@ -14,12 +17,22 @@ import java.time.LocalDate;
 public class App 
 {
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-        CreateTaskRequest request = new CreateTaskRequest();
-        request.setDescription("Learn JDBC");
-        request.setDeadline(LocalDate.now().plusWeeks(2));
+       CreateTaskRequest request = new CreateTaskRequest();
+       request.setDescription("Learn JDBC");
+       request.setDeadline(LocalDate.now().plusWeeks(2));
 
         TaskRepository taskRepository = new TaskRepository();
-        taskRepository.createTask(request);
+       taskRepository.createTask(request);
+
+      UpdateTaskRequest updateRequest = new UpdateTaskRequest();
+        updateRequest.setDone(true);
+
+      taskRepository.updateTask(1, updateRequest);
+
+//        taskRepository.deleteTask(1);
+
+        List<Task> tasks = taskRepository.getTask();
+        System.out.println(tasks);
 
     }
 }
